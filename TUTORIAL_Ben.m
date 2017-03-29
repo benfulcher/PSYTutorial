c1eaafb5f7f2ecc04ae96f59ddf2a29fc6904370
Ben Fulcher <ben.d.fulcher@gmail.com>
2017-03-29 11:29:02 +1100

    Polishing tutorial

--- TUTORIAL_Ben.m
+++ TUTORIAL_Ben.m
@@ -1,25 +1,9 @@
-%% YOUR NAME:
-
-%% assessment for ERP
-% 8 x 1 point --- EXCERCISE (marked as E in this document). Fill in the
-% appropriate location, submit this m-file to the tutor.
-
-% 3 x 1 point --- OPEN ENDED QUESTIONS (marked as Q1-Q3, at the end). Write
-% your answer in this document, submit it to the tutor.
-%
-% 3 x 1 point --- FIGURE SUBMISSION. Use this code to submit figures as .jpg or .png
-% to the tutor. Use channel 187, which was not used in the lecture.
-% For this assessment, choose a cateogry other than upright face.
-
-% DON'T FORGET TO WRITE YOUR NAME AT THE TOP
+% In this tutorial, we'll go through some code to produce some of the
+% time-series analysis techniques we learned from the lecture
 
 % Start with a clear workspace
 clear all
 close all
-
-%% Useful setting for debugging
-dbstop if error
-dbstop if warning
 
 % Set home directory
 % homeDir = '~/Desktop/Psy3310 Intro to comp neuro/15Dec3_IntroCompNeuroscience/'; % **change for your setting**
@@ -28,61 +12,72 @@ dbstop if warning
 %-------------------------------------------------------------------------------
 %% First we're going to generate some signals
 %-------------------------------------------------------------------------------
-% Let's store the different signals in the structure, x
+% We'll store the different signals in the structure, x
 x = struct();
 
 % Let's set the number of samples in each signal:
-numSamples = 1000; % the number of samples in the random noise signal
+numSamples = 1000; % the number of samples in each signal (i.e., time-series length)
 noiseLevel = 1; % the standard deviation of the noise signal
 
 % 1. Random noise
-x.RandomNoise = noiseLevel*randn(numSamples,1); % generate random, Gaussian-distributed values
-
-% We can imagine values in x orderded through time.
+% We can imagine values in the vector, orderded through time.
 % In this case, x is just a set of random numbers, so there should be no
-% correlation structure.
+% structure through time.
+x.RandomNoise = noiseLevel*randn(numSamples,1); % generate random, Gaussian-distributed values
 
 % 2. Pure sinusoid
 % Just a single, pure sin wave
+% This will have values that are correlated through time, unlike the noise
+% (imagine this as a signal that we want to measure and pick up on)
 x.SingleSinusoid = sin((1:numSamples)'/10);
 
 % 3. Pure sinusoid with random noise added:
+% By adding the noise to the sine wave, we can produce a noisy sine wave
 x.SingleSinusoidNoise = x.SingleSinusoid + x.RandomNoise;
 
 % 4. Sum of two sinusoids of different frequencies
 % Let's add two sine waves, one with double the frequency of the other, together
+% Add a faster oscillation to the existing SingleSinusoid signal:
 x.TwoSinusoids = x.SingleSinusoid + sin((1:numSamples)'/20);
 
 % 5. Two sinusoids with noise
-% Let's add random noise to those two sine waves
+% Let's add random noise to those two sine waves to obscure the periodic structure
 x.TwoSinusoidsNoise = x.TwoSinusoids + x.RandomNoise;
 
 %-------------------------------------------------------------------------------
 %% Let's plot each signal in the time domain
 %-------------------------------------------------------------------------------
 % Plot the time series:
-signalTypes = fieldnames(x);
-numSignals = length(signalTypes);
+signalTypes = fieldnames(x); % names of all of the signals we defined above
+numSignals = length(signalTypes); % the number of signals we defined
 
+% Generate a new figure and plot each signal, and title each subplot
 figure('color','w');
 for i = 1:numSignals
     subplot(numSignals,1,i)
     plot(x.(signalTypes{i}),'k');
-    xlabel('Time (sample)'); ylabel('Value')
+    xlabel('Time (sample)');
+    ylabel('Value')
     title(signalTypes{i})
 end
 
-% Explain the components in each signal from their definitions above
+%===============================================================================
+%                       ---QUESTIONS TO THINK ABOUT---
+%===============================================================================
+% Explain the components in each signal from their definitions above.
 % Look at the signals -- do they look like you expect them to?
-% You might like to keep this plot open to compare to other analyses we will do later on
+% You might like to keep this plot open to compare to other analyses we will
+%   do later on.
+%===============================================================================
+
 
 %-------------------------------------------------------------------------------
 %% How does the sinusoid look as we increase the noise level?
-% E1: Run the code below and note what happens to the periodic signal
-% when the noise level increases
-% e.g., on the SingleSinusoidNoise signal
+%-------------------------------------------------------------------------------
+% Set a range of noise levels:
 noiseLevelScale = linspace(0.1,2,40); % increasing noise level
 
+% Plot a sine wave as the noise level increases (for a fixed noise signal):
 f = figure('color','w');
 for i = 1:length(noiseLevelScale)
     plot(x.SingleSinusoid + noiseLevelScale(i)*x.RandomNoise,'-k')
@@ -91,19 +86,13 @@ for i = 1:length(noiseLevelScale)
     pause(0.1)
 end
 
-%-------------------------------------------------------------------------------
-%% Look at the distribution of values in each signal:
-%-------------------------------------------------------------------------------
-figure('color','w');
-for i = 1:numSignals
-    subplot(numSignals,1,i)
-    histogram(x.(signalTypes{i}),'Normalization','Probability');
-    xlabel('Value'); ylabel('Probability density')
-    title(signalTypes{i})
-end
-
-% E1: Say briefly what these pictures mean?
-% E2: Can you interpret them in terms of the time domain shapes
+%===============================================================================
+%                       ---QUESTIONS TO THINK ABOUT---
+%===============================================================================
+% What happens to the periodic signal as you increase the amount of noise?
+% Is there a noise level at which it's impossible to identify the underlying
+% periodic signal?
+%===============================================================================
 
 %-------------------------------------------------------------------------------
 %% Autocorrelation functions
